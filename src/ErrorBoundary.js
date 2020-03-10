7dc0efa60df19ac689f1f8f09b801c388513a7dc
Arsen28 <50929231+Arsen28@users.noreply.github.com>
2020-03-10 23:19:23 +0400

    Add files via upload

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class ErrorBoundary extends React.Component{
+    constructor(props){
+    super(props) 
+    
+    this.state = {
+        hasError:false
+    }
+}
+
+static getDeriveStateFromError(error){
+    return{
+        hasError:true
+    }
+}
+
+
+render(){
+    if(this.state .hasError){
+        return<h1> OOPS ERROR 404</h1>
+    }
+    return this.props.children
+    }
+}   
+
+export default ErrorBoundary
